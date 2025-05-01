65f617602dcf0c5fd8cd93cf1cd5d2d9161a69be
Harsh Vardhan <harshvardhan@Harshs-MacBook-Air.local>
2025-05-01 23:06:05 +0530

    First Program

---
+++ FirstProgram.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class FirstProgram {
+
+    public static void main(String[] args) {
+          System.out.println("First Successful Program again by Hero....");
+    }
+}
